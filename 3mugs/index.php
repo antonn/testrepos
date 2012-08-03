722cc9af1ad4237dd9f8f022fbd26d9b27b3de36
Antony Johnson <antonycjohnson@gmail.com>
2012-08-03 18:56:29 +0530

    Changed index.php

--- 3mugs/index.php
+++ 3mugs/index.php
@@ -2,7 +2,6 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-
 <meta name="Description" content="Information architecture, Web Design, Web Standards." />
 <meta name="Keywords" content="your, keywords" />
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
